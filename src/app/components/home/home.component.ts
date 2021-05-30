86addd2dcb8e1eded3bcf66bf31e7c3e43472ddd
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-05-30 19:06:09 +0530

    Changed TS config and added sort functionality

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Pipe } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 import { APIResponse, Game } from 'src/app/models';
 import { HttpService } from 'src/app/services/http.service';
@@ -9,17 +10,20 @@ import { HttpService } from 'src/app/services/http.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  public sort: string = '';
-  public games: Array<Game> = [];
+export class HomeComponent implements OnInit, OnDestroy {
+  public sort: string;
+  public games: Array<Game>;
+  private routeSub: Subscription;
+  private gameSub: Subscription;
 
   constructor(
     private httpService: HttpService,
+    private router: Router,
     private activatedRoute: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params: Params) => {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
       if (params['game-search']) {
         this.searchGames('metacrit', params['game-search']);
       } else {
@@ -29,12 +33,26 @@ export class HomeComponent implements OnInit {
   }
 
   searchGames(sort: string, search?: string): void {
-    this.httpService
+    this.gameSub = this.httpService
       .getGameList(sort, search)
       .subscribe((gameList: APIResponse<Game>) => {
         this.games = gameList.results;
         console.log(gameList);
         console.log(this.games);
       });
+  }
+
+  openGameDetails(id: string): void {
+    this.router.navigate(['details', id]);
+  }
+
+  ngOnDestroy(): void {
+    if (this.gameSub) {
+      this.gameSub.unsubscribe();
+    }
+
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
   }
 }
