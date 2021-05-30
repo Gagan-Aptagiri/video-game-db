cbeaee40a9c23cb6c42fa320434221e6e9e24cf7
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-05-30 21:01:12 +0530

    Details-component complete and added getGameDetails in httpService

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Game } from 'src/app/models';
+import { ActivatedRoute, Params } from '@angular/router';
+import { HttpService } from '../../services/http.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss'],
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, OnDestroy {
   gameRating = 0;
+  gameId: string;
+  game: Game;
+  routeSub: Subscription;
+  gameSub: Subscription;
 
-  constructor() {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private httpService: HttpService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
+      this.gameId = params['id'];
+      this.getGameDetails(this.gameId);
+    });
+  }
 
   getColor(value: number): string {
     if (value > 75) {
@@ -21,6 +37,27 @@ export class DetailsComponent implements OnInit {
       return '#f7aa38';
     } else {
       return '#ef4655';
+    }
+  }
+
+  getGameDetails(id: string): void {
+    this.gameSub = this.httpService
+      .getGameDetails(id)
+      .subscribe((gameResp: Game) => {
+        this.game = gameResp;
+
+        setTimeout(() => {
+          this.gameRating = this.game.metacritic;
+        }, 1000);
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.gameSub) {
+      this.gameSub.unsubscribe();
+    }
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
     }
   }
 }
